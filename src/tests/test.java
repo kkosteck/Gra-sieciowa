75edb44bd974a02b1cdb80ff45656a8e291568b5
kkosteck <48053724+kkosteck@users.noreply.github.com>
2020-06-14 14:24:01 +0200

    Unit test for collisions

--- src/tests/test.java
+++ src/tests/test.java
@@ -1,25 +1,114 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import main.Game;
 import main.Handler;
-import main.entities.creatures.Player;
+import main.entities.creatures.Creature;
 import main.maps.Map;
-import main.utilities.Utilities.Teams;
 
 class test {
-	
+
+	// entity without texture for collision testing
+	class TestEntity extends Creature {
+
+		public TestEntity(Handler handler, float x, float y, int width, int height) {
+			super(handler, x, y, width, height);
+		}
+
+		@Override
+		public void tick() {
+		}
+
+		@Override
+		public void render(Graphics g) {
+		}
+	}
 
 	@Test
-	void testSolidTiles() {
-
+	void testEntityInEntityCollision() {
 		Handler handler = new Handler(new Game("Game", 1280, 720));
 		Map map = new Map(handler, "/map/basicMap.txt");
 		handler.setMap(map);
-		Player player = new Player(handler, null, 0.0f, 0.0f, "Test", Teams.NONE);
+		TestEntity testEntity = new TestEntity(handler, 0, 0, 20, 20);
+		map.getEntityManager().addEntity(testEntity);
+		Assert.assertFalse(testEntity.checkEntityCollisions(0, 0));
+
+		TestEntity testEntity2 = new TestEntity(handler, 0, 0, 20, 20);
+		map.getEntityManager().addEntity(testEntity2);
+		Assert.assertTrue(testEntity.checkEntityCollisions(0, 0));
+	}
+
+	@Test
+	void testEntityHitBox() {
+		Handler handler = new Handler(new Game("Game", 1280, 720));
+		Map map = new Map(handler, "/map/basicMap.txt");
+		handler.setMap(map);
+		TestEntity testEntity = new TestEntity(handler, 0, 0, 20, 20);
+		map.getEntityManager().addEntity(testEntity);
+		Rectangle testRectangle = new Rectangle(0, 0, 20, 20);
+
+		Assert.assertEquals(testRectangle, testEntity.getCollisionBounds(0, 0));
+		Assert.assertNotEquals(testRectangle, testEntity.getCollisionBounds(1, 1));
+	}
+
+	@Test
+	void testTileCollision() {
+		Handler handler = new Handler(new Game("Game", 1280, 720));
+		Map map = new Map(handler, "/map/basicMap.txt");
+		handler.setMap(map);
+		TestEntity testEntity = new TestEntity(handler, 32, 40, 20, 20);
+		map.getEntityManager().addEntity(testEntity);
+		testEntity.setxMove(-20);
+		testEntity.move();
+		Assert.assertEquals(16, testEntity.getX(), 0);
+		testEntity.setxMove(-20);
+		testEntity.move();
+		Assert.assertEquals(16, testEntity.getX(), 0);
+		testEntity.setxMove(20);
+		testEntity.move();
+		Assert.assertEquals(36, testEntity.getX(), 0);
+	}
+
+	@Test
+	void testMovingEntityCollisionInXAxis() {
+		Handler handler = new Handler(new Game("Game", 1280, 720));
+		Map map = new Map(handler, "/map/basicMap.txt");
+		handler.setMap(map);
+		TestEntity testEntity1 = new TestEntity(handler, 40, 40, 20, 20);
+		map.getEntityManager().addEntity(testEntity1);
+		testEntity1.setxMove(5);
+		TestEntity testEntity2 = new TestEntity(handler, 50, 40, 20, 20);
+		map.getEntityManager().addEntity(testEntity2);
 		
-		Assert.assertTrue(player.checkEntityCollisions(0, 0));
+		testEntity1.move();
+		Assert.assertEquals(40, testEntity1.getX(), 0);
+		
+		testEntity1.setxMove(-10);
+		testEntity1.move();
+		Assert.assertEquals(30, testEntity1.getX(), 0);
+	}
+	
+	@Test
+	void testMovingEntityCollisionInYAxis() {
+		Handler handler = new Handler(new Game("Game", 1280, 720));
+		Map map = new Map(handler, "/map/basicMap.txt");
+		handler.setMap(map);
+		TestEntity testEntity1 = new TestEntity(handler, 40, 40, 20, 20);
+		map.getEntityManager().addEntity(testEntity1);
+		testEntity1.setyMove(5);
+		TestEntity testEntity2 = new TestEntity(handler, 40, 50, 20, 20);
+		map.getEntityManager().addEntity(testEntity2);
+		
+		testEntity1.move();
+		Assert.assertEquals(40, testEntity1.getY(), 0);
+		
+		testEntity1.setyMove(-10);
+		testEntity1.move();
+		Assert.assertEquals(30, testEntity1.getY(), 0);
 	}
 }
